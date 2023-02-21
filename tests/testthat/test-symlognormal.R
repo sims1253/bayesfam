88ccd93f9448b3982f8a95f845bc0ec73386ecb3
Maximilian Scholz <m0hawk@gmx.de>
2023-02-21 11:47:05 +0100

    Fix symlognormal test filename

--- tests/testthat/test-symlognormal.R
+++ tests/testthat/test-symlognormal.R
@@ -1,9 +1,9 @@
 test_that("symlognormal", {
    # Check of brms can fit the custom family and recover the intercept and shape
   expect_brms_family(
-    intercept = symlog(0.5),
-    aux_par = 0.4,
-    ref_intercept = 0.5,
+    intercept = symlog(0),
+    aux_par = 1,
+    ref_intercept = 0,
     parameter_link = symlog,
     rng_link = identity,
     family = symlognormal,
